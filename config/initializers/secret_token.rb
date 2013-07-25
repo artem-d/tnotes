5298dd29782866a15e3150e537a3c95b10da471b
Artem Derevianko <artem.d.6@gmail.com>
2013-07-25 13:34:20 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tombotes::Application.config.secret_key_base = '8bc6190def6beb264d3848edc7b19aa00a8aa3451b3a05d344177c478e844bd8f11851b8dd08cbb7b2642690b4cef718d75f9137e5fd4dfa2035202d47581b24'
